8a73bc9ebff409f31c21439646c49ac294831bc4
Kevin <862860000@qq.com>
2019-01-11 00:38:14 +0800

    add string pad
    添加字符串填充

---
+++ strings/pad_test.go
@@ -0,0 +1,12 @@
+package strings
+
+import "testing"
+
+func TestPadLeft(t *testing.T) {
+	s1 := PadLeft("abc", 3, [1]string{"1"})
+	t.Logf("s1:%s\n", s1)
+}
+func TestPadRight(t *testing.T) {
+	s1 := PadRight("abc", 3, [1]string{"1"})
+	t.Logf("s1:%s\n", s1)
+}
