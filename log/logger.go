028afd48bbd0d453843e7c1e72f67643b3263d16
Kevin <862860000@qq.com>
2019-01-30 16:47:16 +0800

    add logger

---
+++ log/logger.go
@@ -0,0 +1,21 @@
+package log
+
+// Logger logger
+type Logger interface {
+	// Debugf 测试
+	Debugf(format string, args ...interface{})
+	// Debugln 测试
+	Debugln(args ...interface{})
+	// Infof 信息
+	Infof(format string, args ...interface{})
+	// Infoln 消息
+	Infoln(args ...interface{})
+	// Warningf 警告
+	Warningf(format string, args ...interface{})
+	// Warningln 警告
+	Warningln(args ...interface{})
+	// Errorf 错误
+	Errorf(format string, args ...interface{})
+	// Errorln 错误
+	Errorln(args ...interface{})
+}
