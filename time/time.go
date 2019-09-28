f410ceeec71472976ac39075526171e0dfba11a5
Kevin <862860000@qq.com>
2019-09-28 15:10:55 +0800

    udpate time

--- time/time.go
+++ time/time.go
@@ -6,9 +6,8 @@ import (
 
 // TodayStartEndTime 今天开始时间和结束时间
 func TodayStartEndTime() (stime.Time, stime.Time) {
-	today := stime.Now().Format("2006-01-02")
-	layout := "2006-01-02 15:04:05"
-	start, _ := stime.Parse(layout, today+" 00:00:00")
-	end, _ := stime.Parse(layout, today+" 23:59:59")
+	today := stime.Now().Format(DateLayout)
+	start, _ := stime.Parse(DefaultLayout, today+" 00:00:00")
+	end, _ := stime.Parse(DefaultLayout, today+" 23:59:59")
 	return start, end
 }
