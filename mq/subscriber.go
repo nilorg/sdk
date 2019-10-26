69f2fafa802ab636dfda8494df7a493bf82a5290
Kevin <862860000@qq.com>
2019-10-26 19:34:58 +0800

    add sub struct

--- mq/subscriber.go
+++ mq/subscriber.go
@@ -9,3 +9,18 @@ type SubscribeHandler func(ctx context.Context, msg interface{})
 type Subscriber interface {
 	Register(topic string, h SubscribeHandler, queue ...string) error
 }
+
+type subscriber struct {
+	client Clienter
+}
+
+func (s *subscriber) Register(topic string, h SubscribeHandler, queue ...string) error {
+	return s.client.Subscribe(topic, h, queue...)
+}
+
+// NewSubscriber ...
+func NewSubscriber(client Clienter) Subscriber {
+	return &subscriber{
+		client: client,
+	}
+}
