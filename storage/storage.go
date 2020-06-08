12ccd04c853072783b4666c18768d22e1b4f3a08
Kevin <862860000@qq.com>
2020-06-08 16:14:38 +0800

    add storage

---
+++ storage/storage.go
@@ -0,0 +1,49 @@
+package storage
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// Storager 存储
+type Storager interface {
+	Upload(read io.Reader, filename string) (err error)
+	Download(write io.Writer, filename string) (err error)
+}
+
+// DefaultStorage 默认存储
+type DefaultStorage struct {
+	BasePath string
+}
+
+// NewDefaultStorage 创建默认存储
+func NewDefaultStorage() *DefaultStorage {
+	return &DefaultStorage{
+		BasePath: "./",
+	}
+}
+
+// Upload 上传
+func (ds *DefaultStorage) Upload(read io.Reader, filename string) (fullPath string, err error) {
+	fullPath = filepath.Join(ds.BasePath, filename)
+	dst, err := os.Create(fullPath)
+	if err != nil {
+		return
+	}
+	_, err = io.Copy(dst, read)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// Download 下载
+func (ds *DefaultStorage) Download(dist io.Writer, filename string) (err error) {
+	fullPath := filepath.Join(ds.BasePath, filename)
+	file, err := os.Open(fullPath)
+	if err != nil {
+	}
+	_, err = io.Copy(dist, file)
+	return
+}
