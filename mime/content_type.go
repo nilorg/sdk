2c673b72d02cfb1b676787f1c08dd3ff3d099f56
Kevin <862860000@qq.com>
2020-06-10 15:56:10 +0800

    Update content_type.go

--- mime/content_type.go
+++ mime/content_type.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// GetFileContentType 获取文件流的Content
+// GetFileContentType 获取文件流的ContentType
 func GetFileContentType(r io.Reader) (contentType string, err error) {
 	// 只需要前 512 个字节就可以了
 	buffer := make([]byte, 512)
