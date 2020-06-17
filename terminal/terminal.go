89c9d12253a34310b7910545a2b5abaadac7f9ec
Kevin <862860000@qq.com>
2020-06-17 10:47:44 +0800

    Update terminal.go

--- terminal/terminal.go
+++ terminal/terminal.go
@@ -5,8 +5,13 @@ import (
 	"os"
 )
 
-// Title 设置终端标题
-func Title(format string, a ...interface{}) {
+// Titlef 设置终端标题
+func Titlef(format string, a ...interface{}) {
 	format = fmt.Sprintf("\033]0;%s\007", format)
 	fmt.Fprintf(os.Stdout, format, a...)
+}
+
+// Title 设置终端标题
+func Title(title string) {
+	Titlef(title)
 }
