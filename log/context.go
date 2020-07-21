41ffd7796cc872237d59eca7fc364458938032e5
Kevin <862860000@qq.com>
2020-07-21 15:47:16 +0800

    Create context.go

---
+++ log/context.go
@@ -0,0 +1,42 @@
+package log
+
+import "context"
+
+var (
+	traceIDKey struct{}
+	spanIDKey  struct{}
+	userIDKey  struct{}
+)
+
+// NewTraceIDContext ...
+func NewTraceIDContext(ctx context.Context, traceID string) context.Context {
+	return context.WithValue(ctx, traceIDKey, traceID)
+}
+
+// FromTraceIDContext ...
+func FromTraceIDContext(ctx context.Context) (traceID string, ok bool) {
+	traceID, ok = ctx.Value(traceIDKey).(string)
+	return
+}
+
+// NewSpanIDContext ...
+func NewSpanIDContext(ctx context.Context, spanID string) context.Context {
+	return context.WithValue(ctx, spanIDKey, spanID)
+}
+
+// FromSpanIDContext ...
+func FromSpanIDContext(ctx context.Context) (spanID string, ok bool) {
+	spanID, ok = ctx.Value(spanIDKey).(string)
+	return
+}
+
+// NewUserIDContext ...
+func NewUserIDContext(ctx context.Context, userID string) context.Context {
+	return context.WithValue(ctx, userIDKey, userID)
+}
+
+// FromUserIDContext ...
+func FromUserIDContext(ctx context.Context) (userID string, ok bool) {
+	userID, ok = ctx.Value(userIDKey).(string)
+	return
+}
