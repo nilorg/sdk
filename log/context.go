d78210c90563bf6066fbf13768a007d92b01542c
Kevin <862860000@qq.com>
2020-07-22 13:19:21 +0800

    fix context

--- log/context.go
+++ log/context.go
@@ -2,7 +2,7 @@ package log
 
 import "context"
 
-var (
+type (
 	traceIDKey struct{}
 	spanIDKey  struct{}
 	userIDKey  struct{}
@@ -10,33 +10,33 @@ var (
 
 // NewTraceIDContext ...
 func NewTraceIDContext(ctx context.Context, traceID string) context.Context {
-	return context.WithValue(ctx, traceIDKey, traceID)
+	return context.WithValue(ctx, traceIDKey{}, traceID)
 }
 
 // FromTraceIDContext ...
 func FromTraceIDContext(ctx context.Context) (traceID string, ok bool) {
-	traceID, ok = ctx.Value(traceIDKey).(string)
+	traceID, ok = ctx.Value(traceIDKey{}).(string)
 	return
 }
 
 // NewSpanIDContext ...
 func NewSpanIDContext(ctx context.Context, spanID string) context.Context {
-	return context.WithValue(ctx, spanIDKey, spanID)
+	return context.WithValue(ctx, spanIDKey{}, spanID)
 }
 
 // FromSpanIDContext ...
 func FromSpanIDContext(ctx context.Context) (spanID string, ok bool) {
-	spanID, ok = ctx.Value(spanIDKey).(string)
+	spanID, ok = ctx.Value(spanIDKey{}).(string)
 	return
 }
 
 // NewUserIDContext ...
 func NewUserIDContext(ctx context.Context, userID string) context.Context {
-	return context.WithValue(ctx, userIDKey, userID)
+	return context.WithValue(ctx, userIDKey{}, userID)
 }
 
 // FromUserIDContext ...
 func FromUserIDContext(ctx context.Context) (userID string, ok bool) {
-	userID, ok = ctx.Value(userIDKey).(string)
+	userID, ok = ctx.Value(userIDKey{}).(string)
 	return
 }
