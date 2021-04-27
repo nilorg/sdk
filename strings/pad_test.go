019c9f9f1e6c1668f785d155c8228661b0f91617
Kevin <862860000@qq.com>
2021-04-27 18:21:28 +0800

    modify string pad

--- strings/pad_test.go
+++ strings/pad_test.go
@@ -3,10 +3,10 @@ package strings
 import "testing"
 
 func TestPadLeft(t *testing.T) {
-	s1 := PadLeft("abc", 3, [1]string{"1"})
+	s1 := PadLeft("abc", 3, "1")
 	t.Logf("s1:%s\n", s1)
 }
 func TestPadRight(t *testing.T) {
-	s1 := PadRight("abc", 3, [1]string{"1"})
+	s1 := PadRight("abc", 3, "1")
 	t.Logf("s1:%s\n", s1)
 }
